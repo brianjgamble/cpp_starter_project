ee1d585efd0a5c9a08c3c2ba7ada8f0ddc696cf2
Brian Gamble <brianjgamble@users.noreply.github.com>
2022-06-11 10:25:34 -0500

    Switch from doctest to catch2 3.0.1

--- test/test_example.cpp
+++ test/test_example.cpp
@@ -1,14 +1,14 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
+#include <catch2/catch_all.hpp>
 
 int factorial(int number) {
     return number > 1 ? factorial(number - 1) * number : 1;
 }
 
 TEST_CASE("testing the factorial function") {
-    CHECK(factorial(0) == 1);
-    CHECK(factorial(1) == 1);
-    CHECK(factorial(2) == 2);
-    CHECK(factorial(3) == 6);
-    CHECK(factorial(10) == 3628800);
+    REQUIRE(factorial(0) == 1);
+    REQUIRE(factorial(1) == 1);
+    REQUIRE(factorial(2) == 2);
+    REQUIRE(factorial(3) == 6);
+    REQUIRE(factorial(10) == 3628800);
 }
